5a035d08120c5f0f06f315e6319af9bb6d4bf79c
Wei Xiaohai <wistoch@gmail.com>
2017-08-31 10:47:47 +0800

    fix bugs

--- webaudio/room-effects-sample.js
+++ webaudio/room-effects-sample.js
@@ -154,10 +154,10 @@ RoomEffectsSample.prototype.releaseToSpeak = function(event) {
 
     this.convolver.buffer = this.impulseResponseBuffer;
     // Connect the graph.
-   // this.source.connect(this.convolver);
-   // this.convolver.connect(context.destination);
+    this.source.connect(this.convolver);
+    this.convolver.connect(context.destination);
 
-    this.source.connect(context.destination);
+    //this.source.connect(context.destination);
     // Save references to important nodes.
 
 
